a6f854ebb3206c4a0d88063eb4c7e673ced13ec6
heno-1010 <heno-1010@users.noreply.github.com>
2026-02-14 16:40:17 +0900

    SnippetManager.csでsnippetのnullチェックと追加

--- SnippetEngine.Core/Services/SnippetManager.cs
+++ SnippetEngine.Core/Services/SnippetManager.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using SnippetEngine.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SnippetEngine.Core.Services
 {
-    internal class SnippetManager
+    public class SnippetManager
     {
+        private List<Snippet> _snippets = new();
+
+        public void AddSnippet(Snippet snippet)
+        {
+            ArgumentNullException.ThrowIfNull(snippet); //snippetがnullだったときはエラーを返す
+            _snippets.Add(snippet);
+        }
     }
 }
