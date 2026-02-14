5c9b7fd6d9fafe7bbd04a25b1c4674a5dc21cc37
heno-1010 <heno-1010@users.noreply.github.com>
2026-02-14 16:17:25 +0900

    Snippet.csの追加

---
+++ SnippetEngine.Core/Models/Snippet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnippetEngine.Core.Models
+{
+    public class Snippet
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+    }
+}
