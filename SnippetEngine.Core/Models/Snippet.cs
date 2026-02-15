5f1c527d96b2819eda8eef175c14240e0145c4ad
heno-1010 <heno-1010@users.noreply.github.com>
2026-02-15 18:37:02 +0900

    検索機能追加

--- SnippetEngine.Core/Models/Snippet.cs
+++ SnippetEngine.Core/Models/Snippet.cs
@@ -6,9 +6,28 @@ namespace SnippetEngine.Core.Models
 {
     public class Snippet
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Code { get; set; }
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Code { get; private set; }
+
+        private void SetValues(string title, string description, string code)
+        {
+            if (string.IsNullOrWhiteSpace(title)) // titleがnull、空白の時にエラーを返す
+                throw new ArgumentException("Title is required.");
+            Title = title;
+            Description = description ?? "";
+            Code = code ?? "";
+        }
+
+        public Snippet(string title,string description,string code)
+        {
+            SetValues(title,description,code);
+        }
+
+        public void Update(string title,string description,string code)
+        {
+            SetValues(title, description, code);
+        }
     }
 }
--- SnippetEngine.Core/Services/SnippetManager.cs
+++ SnippetEngine.Core/Services/SnippetManager.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Net.WebRequestMethods;
 
 namespace SnippetEngine.Core.Services
 {
@@ -14,5 +15,21 @@ namespace SnippetEngine.Core.Services
             ArgumentNullException.ThrowIfNull(snippet); //snippetがnullだったときはエラーを返す
             _snippets.Add(snippet);
         }
+        public void RemoveSnippet(Snippet snippet)
+        {
+            _snippets.Remove(snippet);
+        }
+        public IReadOnlyList<Snippet> GetAll()
+        {
+            return _snippets.AsReadOnly();
+        }
+        public IEnumerable<Snippet> Search(string keyword)
+        {
+            if (string.IsNullOrWhiteSpace(keyword)) // keywordがnull、空白の時
+                return GetAll();
+
+            return _snippets.Where(s => s.Title.IndexOf(keyword,StringComparison.OrdinalIgnoreCase) >= 0 || s.Description.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0 || s.Code.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
     }
 }
