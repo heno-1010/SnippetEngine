7190f50fcc5653e96086e5039a1d4345cd32ea08
heno-1010 <heno-1010@users.noreply.github.com>
2026-03-08 18:33:00 +0900

    MainViewModel作成

--- SnippetEngine/MainWindow.xaml.cs
+++ SnippetEngine/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SnippetEngine.ViewModels;
 
 namespace SnippetEngine
 {
@@ -19,6 +20,7 @@ namespace SnippetEngine
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ SnippetEngine/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using SnippetEngine.Core.Models;
+using System.Text;
+
+namespace SnippetEngine.ViewModels
+{
+    class MainViewModel
+    {
+        public ObservableCollection<Snippet> Snippets { get; set; }
+
+        public MainViewModel()
+        {
+            Snippets = new ObservableCollection<Snippet>()
+            {
+                new Snippet("Title","Description","Code"),
+                new Snippet("Title2","Description2","Code2")
+            };
+        }
+    }
+}
